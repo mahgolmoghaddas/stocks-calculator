34367bf8fe54e7def5774928c2c9e60cc21614a9
Mahgol77 <39947946+mahgolmoghaddas@users.noreply.github.com>
2025-03-29 21:42:29 -0400

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../assets/investment-calculator-logo.png'
+
+function Header() {
+  return (
+    <div id='header'>
+        <img src={logo} alt='something'/>
+        <h1>Investment Calculator</h1>
+    </div>
+
+  )
+}
+
+export default Header
